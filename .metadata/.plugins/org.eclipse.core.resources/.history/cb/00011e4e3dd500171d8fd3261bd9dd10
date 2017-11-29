ed2853e7dde30f50a60b82b56f1fe551396f7d76
LiuEdward <LiuE1309@sduhsd.lan>
2017-11-29 11:50:21 -0800

    first assignment

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cb/00011e4e3dd500171d8fd3261bd9dd10
@@ -0,0 +1,17 @@
+
+public class FirstProg
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
--- ECSJavaProject/src/FirstProg.java
+++ ECSJavaProject/src/FirstProg.java
@@ -1,9 +1,17 @@
-
+//Comment for my FirstProg
+//Just prints out a message to the display
+/*
+ efdsssdfdgfdfgdsfg
+ adfsdfg
+ fdgsfgd
+ fgregfd
+ glhgsfjdk
+ */
 public class FirstProg
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		System.out.println("Mr. Cummings is cool!");
 		
 		
 		

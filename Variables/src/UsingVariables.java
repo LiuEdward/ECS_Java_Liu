8df381eec8f152c1a0ce6f4ee4fa17b0b9192558
LiuEdward <LiuE1309@sduhsd.lan>
2017-11-30 11:05:01 -0800

    assignment 2 part 2

---
+++ Variables/src/UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables 
+{
+	public static void main(String[] args)
+	{
+		int  x;
+		double y;
+		String z;
+		
+		x=113;
+		y=2.71828;
+		z="Computer Science";
+		System.out.println("This is room # "+ x);
+		System.out.println("e is close to "+ y);
+		System.out.println("I am learning a bit about "+ z);
+	}
+
+}

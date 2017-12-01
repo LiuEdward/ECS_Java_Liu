4ba19c8712092c3c5ec077bf93e263897db11724
LiuEdward <LiuE1309@sduhsd.lan>
2017-12-01 10:09:39 -0800

    assignment 2 part 4

---
+++ Variables/src/StillUsingVariables.java
@@ -0,0 +1,11 @@
+public class StillUsingVariables
+{
+	public static void main(String[] args)
+	{
+		int yourgradeyear;
+		String yourname;
+		yourgradeyear=2021;
+		yourname="Edward";
+		System.out.println("My name is "+yourname+" and I'll graduate in "+yourgradeyear+".");
+	}
+}

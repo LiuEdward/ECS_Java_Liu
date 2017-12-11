26b69f4abfd36b339260bbd5dfcc9415f34da837
LiuEdward <LiuE1309@sduhsd.lan>
2017-12-11 11:11:55 -0800

    assignment 6

---
+++ While Loops/src/CollatzSequence.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CollatzSequence 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Let's create a COLLAT SEQUENCE!");
+		System.out.println("Rules: ");
+		System.out.println("If the number is even, divide it by two.");
+		System.out.println("If the number is odd, multiply three and add one.");
+	}
+}
